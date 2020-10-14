7ceb4cbf8649adc57955a8e78dba210be5879d7a
Dima <SemenihinDmit@yandex.ru>
2020-10-14 21:11:33 +0300

    added fields and costructors

---
+++ List.java
@@ -0,0 +1,18 @@
+package Withlist;
+
+public class List {
+    class item {
+        private int value;
+        private item next;
+
+
+        public item(int value) {
+            this.value = value;
+            this.next = null;
+        }
+
+
+        
+    }
+
+}
